3e4ac8d3f926ef0d84f5f29e5dd52ef6451c2409
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-23 17:20:55 +0330

    .

--- components/Hero.js
+++ components/Hero.js
@@ -3,9 +3,15 @@ import Image from "next/image";
 const Hero = () => {
     return (
         <div className="w-full relative items-center gap-10">
-            <div className='w-full min-h-[100px] h-[35vh] md:h-[65vh] relative '>
-                <img src="/images/cargo-image3.png" alt="" className=" object-cover
-                  w-full h-full z-1 absolute align-top" />
+            <div className='w-full min-h-[100px] h-[35vh] md:h-[65vh] relative'>
+                <Image
+                    src="/images/cargo-image3.png"
+                    alt="Cargo Image"
+                    fill
+                    priority={true}
+                    style={{ objectFit: 'cover' }}
+                    className="z-1 absolute"
+                />
                 <div className="z-2 w-full h-full absolute bg-black opacity-45"></div>
                 <div className="z-3 absolute w-full h-full flex justify-center items-center">
                     <h1 className="mt-24 text-white rounded-lg p-3">
