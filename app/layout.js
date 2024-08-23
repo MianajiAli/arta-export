f3ca91f3404f9a653b56eccf975184fb2ab93f9b
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-23 15:23:14 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa">
+      <meta name="robots" content="noindex,nofollow" />
       <body className={inter.className} >
         <div className='font-medium bg-back flex flex-col relative'>
           <Header />
