06a80d00250e8537aea5992a3f913a03d61b18a9
MianajiAli <87234097+MianajiAli@users.noreply.github.com>
2024-08-25 20:00:19 +0330

    Update RandomProductGrid.js

--- components/RandomProductGrid.js
+++ components/RandomProductGrid.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import ProductCard from '@/components/ProductCard';
 import { fetchProducts } from '@/lib/api';
+import Link from 'next/link';
 
 const RandomProductGrid = () => {
     const [randomProducts, setRandomProducts] = useState([]);
@@ -29,11 +30,15 @@ const RandomProductGrid = () => {
         <div className="w-full px-[10%]" dir="rtl">
             <div className="flex justify-between items-center">
                 <h2 className="text-text1 ">برخی از محصولات</h2>
-                <p className="text-text2">دیدن همه</p>
+                <Link href="/products" >
+                    <p className="text-text2">دیدن همه</p>
+                </Link>
             </div>
             <div className="w-full flex gap-5 justify-center items-center flex-wrap  my-7">
                 {randomProducts.map((product) => (
-                    <ProductCard key={product.id} product={product} onClick={() => { }} />
+                    <Link key={product.id} href="/products" >
+                        <ProductCard product={product} onClick={() => { }} />
+                    </Link>
                 ))}
             </div>
         </div>
